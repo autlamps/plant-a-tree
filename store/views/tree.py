487dd1f87f4f76feeb9237a1c2aabf8f0a916de2
Unknown <dominic@dbp.kiwi>
2018-09-28 11:36:34 +1200

    Fix for Hayden
    * Changed content is tree.py from all variable to just tree
    + Added Growth and Maintenance display
    - Removed .body-min in style

--- store/views/tree.py
+++ store/views/tree.py
@@ -8,17 +8,5 @@ from store.models import Tree
 def get_tree(request, item_id):
     item = get_object_or_404(Tree, pk=item_id)
     return render(request, 'tree.html', context={
-        'name': item.name,
-        'maintenance': item.maintenance,
-        'growth': item.growth_rate,
-        'category': item.category,
-        'description': item.description,
-        'facts': item.fun_facts,
-        'height': item.height,
-        'y1': item.year_one,
-        'y2': item.year_two,
-        'y3': item.year_three,
-        'y5': item.year_five,
-        'y10': item.year_ten,
         'tree': item,
     })
