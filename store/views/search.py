fa09731b31aa82963d2b3217a48548484ef1b047
Unknown <dominic@dbp.kiwi>
2018-10-11 21:15:11 +1300

    Fixed search.py to work if nothing is entered
    As title

--- store/views/search.py
+++ store/views/search.py
@@ -6,16 +6,17 @@ from store.models import Tree, Product
 
 @require_http_methods(["GET"])
 def get_item(request):
-    queries = request.GET.get("words").split()
-
+    queries = request.GET.get("words")
     trees = set()
     products = set()
-    for query in queries:
-        trees.update(Tree.objects.filter(name__icontains=query))
-        trees.update(Tree.objects.filter(description__icontains=query))
+    if queries is not None:
+        queries = queries.split()
+        for query in queries:
+            trees.update(Tree.objects.filter(name__icontains=query))
+            trees.update(Tree.objects.filter(description__icontains=query))
 
-        products.update(Product.objects.filter(name__icontains=query))
-        products.update(Product.objects.filter(description__icontains=query))
+            products.update(Product.objects.filter(name__icontains=query))
+            products.update(Product.objects.filter(description__icontains=query))
 
     return render(request, 'search.html', context={
         'trees': trees,
