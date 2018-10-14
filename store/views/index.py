81641cfd8a8a345a94c2f830ed955c259ad3acdd
Unknown <dominic@dbp.kiwi>
2018-10-14 22:36:31 +1300

    Hot fix for index.html and index.py

--- store/views/index.py
+++ store/views/index.py
@@ -1,8 +1,40 @@
+import random
+
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from store.models import Tree
+
 
 def index_view(request):
-    return render(request, 'index.html')
+    min = 1
+    max = Tree.objects.count()
+    if min == max:
+        tree_number = min
+    else:
+        tree_number = random.randint(min, max)
 
+    tree = Tree.objects.get(id=tree_number)
 
+    if max > 5:
+        num_trees = 5
+    else:
+        num_trees = max
+
+    facts = set()
+    for i in range(num_trees):
+        facts.update(Tree.objects.filter(id=random.randint(min, max)))
+        if len(facts) is not i:
+            num_trees + 1
+
+    trees = set()
+
+    trees.update(Tree.objects.all())
+
+    return render(request, 'index.html', context={
+        'tree': tree,
+        'trees': trees,
+        'facts': facts,
+        'page_title': "Home"
+    })
